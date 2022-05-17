08bcdc1bad4a233dbaa3b142ea22337449ec0aa7
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-05-17 18:46:11 -0300

    Created components and service api, and add anular material for designe base.

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+  getCurrencyData() {
+
+  }
+
+  getTrendingCurrency() {
+
+  }
+
+  getGraphicalCurrencyData() {
+
+  }
+
+  getCurrencyById() {
+
+  }
+}
