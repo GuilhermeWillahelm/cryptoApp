7833a94731a17fcc7956934ae5b8b108b8b3a4d9
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-05-17 19:05:12 -0300

    Created all methods service api

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -6,21 +6,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ApiService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getCurrencyData() {
-
+  getCurrencyData(currency: string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&sparkline=false`)
   }
 
-  getTrendingCurrency() {
-
+  getTrendingCurrency(currency: string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`);
   }
 
-  getGraphicalCurrencyData() {
-
+  getGraphicalCurrencyData(coinId: string, currency: string, days: number) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency}&days=${days}`);
   }
 
-  getCurrencyById() {
-
+  getCurrencyById(coinId: string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${coinId}`);
   }
 }
