34a945223e555fe5445c54a5f397fd027eeaefca
GuilhermeWillahelm <guilhermewlm@outlook.com>
2022-05-18 15:28:33 -0300

    Adding adequacy deploy for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+// import exress
+const express = require('express');
+// start express
+const app = express();
+// folder build
+const appName = 'crypto-verifyapp';
+// builg generate files
+const outputPath = `${__dirname}/dist/${appName}`;
+
+//setter folder of build of content angular
+app.use(express.static(outputPath));
+//redirect requests of index
+app.get('/*', (req, res) => {
+    res.sendFile(`${outputPath}/index.html`);
+});
+//listen port heroku
+app.listen(process.env.PORT);
